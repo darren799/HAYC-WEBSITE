d09f3ee4adc1c61d80fbad057f4ef00b4777e5fc
darren799 <darren@dmvboa.org>
2026-01-03 22:20:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Hamburger menu toggle
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
 const track = document.querySelector('.carousel-track');
 const slides = Array.from(track.children);
 const nextBtn = document.querySelector('.carousel-btn.next');
